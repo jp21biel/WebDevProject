d8d36f89c7b2e7d8c809f41452eee30f5241e869
jp21biel <jp21biel@siena.edu>
2026-04-22 16:18:28 +0000

    Add Codelab

--- public/script.js
+++ public/script.js
@@ -1,4 +1,16 @@
 const remove = document.createElement("button");
-document.getElementById("submit").addEventListener("click", function(){
+remove.className = "remove";
+remove.value = "-";
+function rmv(e){
+    let parent = e.parent;
+    
+}
+function add(){
     let item = document.createElement("p", document.getElementById("ingr").value);
-});
+    item.className = "ingredient";
+    let div = document.createElement("div");
+    div.appendChild(item);
+    div.appendChild(remove);
+    document.getElementById("chosen").appendChild(div);
+}
+document.getElementById("submit").addEventListener("click", add);
